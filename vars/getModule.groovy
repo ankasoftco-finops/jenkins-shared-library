03ecaa2fa164ea681c2d774a2fc0c9b08f3c244a
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-10-02 14:25:40 +0300

    fixed

--- vars/getModule.groovy
+++ vars/getModule.groovy
@@ -2,6 +2,8 @@ def call(String fileName,String moduleName) {
     def bas =  "module "+module_name+" }"
     def son = "# end "+moduleName
     def file = readFile(fileName)
+    echo file
+    echo bas
     def modBasIndeks = file.indexOf(bas)
     def modSonIndeks = file.indexOf(son)
     def currentConfig = ""
