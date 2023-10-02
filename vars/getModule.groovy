70f8594c951df91552796eb82c249effda02edbd
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-10-02 14:18:33 +0300

    fixed

--- vars/getModule.groovy
+++ vars/getModule.groovy
@@ -1,9 +1,10 @@
-def call(String fileName,String moduleName) {
+String call(String fileName,String moduleName) {
     def bas =  "module "+module_name+" }"
     def son = "# end "+moduleName
     def file = readFile(file)
     def modBasIndeks = file.indexOf(bas)
     def modSonIndeks = file.indexOf(son)
+    def currentConfig = ""
     
     if (modBasIndeks != -1 && modSonIndeks != -1) {
         currentConfig = file.substring(modBasIndeks, modSonIndeks + endModule.length()) 
