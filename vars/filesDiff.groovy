faf072fbc9eaba96ccab98bd7eb275b0789fddab
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-10-02 13:34:06 +0300

    edit

--- vars/filesDiff.groovy
+++ vars/filesDiff.groovy
@@ -2,5 +2,5 @@ import com.ofu.FileHelper
 
 String call(File currentConfig, File newConfig, String moduleName) {
     def fileHelper = new FileHelper()
-    return fileHelper.substringString("module "+ moduleName+ " {", "# end" + moduleName )
+    return fileHelper.substringString("module "+ moduleName+ " {", "# end " + moduleName )
 }
