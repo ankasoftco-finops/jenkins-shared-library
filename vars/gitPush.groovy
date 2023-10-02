7e9839b7361fa3bd788a80f491d869811b1b2859
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-10-02 09:30:11 +0300

    new feature added

---
+++ vars/gitPush.groovy
@@ -0,0 +1,10 @@
+def call(String message) {    
+    sh """
+        git add .
+        git diff-index --quiet HEAD || \
+        git \
+            -c user.name=\"omerulusoy41\" \
+            -c user.email=\"ulusoyomerfaruk29@gmail.com\" \
+            commit -m "${message}"
+    """
+}
