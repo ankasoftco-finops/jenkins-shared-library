25c5b86b1ad8996af230ab50c1c8d7562c86b261
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-10-02 09:05:35 +0300

    new faetures added

--- vars/checkAccountID.groovy
+++ vars/checkAccountID.groovy
@@ -1,8 +1,10 @@
-void call(String accountId, String accountName){
+void call(String accountId){
     accountIdRegex = '^[0-9]+$'
     if (!(accountId =~ accountIdRegex)){
         def message = "account id '${accountId}' does not match regex for account ids"
         echo message
         error message
+    }else{
+        echo "account id '${accountId}' matches regex for account ids"
     }
 }
