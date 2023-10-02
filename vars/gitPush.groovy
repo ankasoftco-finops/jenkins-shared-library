555736ab66ac4d063d8a023bdf908b49a2c49081
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-10-02 09:49:23 +0300

    git push fixed

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -1,11 +1,13 @@
 def call(String message) {    
     sh """
+        git checkout dev
+        git branch
         git add .
         git diff-index --quiet HEAD || \
         git \
             -c user.name=\"omerulusoy41\" \
             -c user.email=\"ulusoyomerfaruk29@gmail.com\" \
             commit -m "${message}"
-        git push origin dev
+        git push -u origin dev
     """
 }
