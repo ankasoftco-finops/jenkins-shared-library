2a19443857b32f6bb0442fac7da1e44b7fe3ec45
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-10-02 09:13:29 +0300

    new feature added

---
+++ vars/gitClone.groovy
@@ -0,0 +1,19 @@
+def git_clone(String giturl, String branch = "dev") {
+    retry(5) {
+        try {
+            timeout(time: 1, unit: "MINUTES") {
+                checkout([$class: "GitSCM",
+                    branches: [[name: "*/${branch}"]],
+                    extensions: [[$class: 'SubmoduleOption', parentCredentials: true, recursive: true], [$class: 'CleanBeforeCheckout', deleteUntrackedNestedRepositories: true]],
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[credentialsId: 'github', url: giturl]]
+                ])
+            }
+        } catch (FlowInterruptedException e) {
+            error("Timeout has been exceeded")
+        } catch (Exception e) {
+            sleep(time:5,unit:"SECONDS")
+            error("Something went wrong during git clone: " + e)
+        }
+    }
+}
