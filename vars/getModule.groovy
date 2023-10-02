c874c0248701bd2c5f05591c04fc0b8e9c357b8f
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-10-02 14:32:23 +0300

    getModule feature added

--- vars/getModule.groovy
+++ vars/getModule.groovy
@@ -1,15 +1,10 @@
 def call(String fileName,String moduleName) {
-    def bas =  "module "+module_name+" {"
+    def bas =  "module \""+module_name+"\" {"
     def son = "# end "+moduleName
     def file = readFile(fileName)
     def modBasIndeks = file.indexOf(bas)
     def modSonIndeks = file.indexOf(son)
     def currentConfig = ""
-    echo bas
-    echo son 
-
-    echo "${modBasIndeks}"
-    echo "${modSonIndeks}"
     if (modBasIndeks != -1 && modSonIndeks != -1) {
         currentConfig = file.substring(modBasIndeks, modSonIndeks + endModule.length()) 
     }
